be43bd4db2516f743b4fa161bd8d0a0f8573684d
jeoffreyduke <85391775+jeoffreyduke@users.noreply.github.com>
2021-10-11 09:29:36 +0100

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h4>Built by Jeoffrey Duke</h4>
+    </div>
+  );
+}
+
+export default Footer;
